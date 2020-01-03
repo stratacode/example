295e889a44db30dbdbc38710cda58d2213a4d57d
Jeff Vroom <jeff@jvroom.com>
2020-01-02 23:14:18 -0800

    fixed path to sc.jar

--- example/simpleParser/run.sh
+++ example/simpleParser/run.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-SCJAR=/jjv/vbuild/bin/sc.jar 
+SCC_CMD=`which scc`
+if [ "$SCC_CMD" = "" ] ; then
+  echo "scc command not found in path"
+  exit 1
+fi
+
+SC_BIN_DIR=`dirname $SCC_CMD`
+SC_JAR=${SC_BIN_DIR}/sc.jar
+
 mkdir -p build
-javac -d build src/sc/simpleParser/SimpleParser.java -classpath $SCJAR 
-java -classpath build:$SCJAR sc.simpleParser.SimpleParser -srcPath testSrc "$@"
+javac -d build src/sc/simpleParser/SimpleParser.java -classpath $SC_JAR 
+java -classpath build:$SC_JAR sc.simpleParser.SimpleParser -srcPath testSrc "$@"
